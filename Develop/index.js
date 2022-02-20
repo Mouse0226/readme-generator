10c111a3cff5711712f6e56cee4bb8453590ac2b
Mouse0226 <mouse1987.ml@gmail.com>
2022-02-20 11:20:13 -0600

    Completed Project

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,147 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const inquirer = require('inquirer');
+const { generateMarkdown, licenseNames } = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create a function to initialize app
-function init() {}
+const promptReadMe = () => {
+   return inquirer.prompt([
+       {
+           type: 'input',
+           name: 'title',
+           message: 'What is the title of your project?',
+           validate: titleInput => {
+               if (titleInput) {
+                   return true;
+               } else {
+                   console.log('Invalid input, please try again.');
+                   return false;
+               }
+           }
+       },
+       {
+           type: 'input',
+           name: 'description',
+           message: 'Please enter a short description of your project',
+           validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Invalid input, please try again.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide instructions to install your project',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                     console.log('Invalid input, please try again.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions on how to use your project',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Invalid input, please try again.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Please provide instructions on how other users can contribute',
+            validate: contributeInput => {
+                if (contributeInput) {
+                    return true;
+                } else {
+                    console.log('Invalid input, please try again.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide instructions on how to test your project',
+            validate: testsInput => {
+                if (testsInput) {
+                    return true;
+                } else {
+                    console.log('Invalid input, please try again.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'licenses',
+            message: 'Please select a license to use.',
+            choices: licenseNames
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub user Name',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Invalid input, please try again.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Invalid input, please try again.');
+                    return false;
+                }
+            }
+        }
+   ]);
+}
 
-// Function call to initialize app
-init();
+// TODO: Create a function to write README file
+const writeToFile = (readMe) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', readMe, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Readme Created!'
+            });
+        }); 
+    });
+};
+
+promptReadMe()
+    .then((readmeData) => {
+        const readMeFile = generateMarkdown(readmeData)
+        return writeToFile(readMeFile);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+    })
+    .catch (err => {
+        console.log(err);
+    })
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,99 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenses = [
+  {
+    name: "GNU AGPLv3",
+    link: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
+  },
+  {
+    name: "GNU GPLv3",
+    link: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  },
+  {
+    name: "GNU LGPLv3",
+    link: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
+  },
+  {
+    name: "Mozilla Public License 2.0",
+    link: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  },
+  {
+    name: "Apache License 2.0",
+    link: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  },
+  {
+    name: "MIT License",
+    link: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  },
+  {
+    name: "Boost Software Licesnse 1.0",
+    link: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  },
+  {
+    name: "Unilicense",
+    link: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  }
+];
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to splice license names out of objects
+const getLicenseNames = (license) => {
+  return license.name;
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Map license names into array for node execution to display as choices
+const licenseNames = licenses.map(getLicenseNames);
+
+// Pair the license selected by the user to the link
+getLicenseLink = (userInput) => {
+  for (let i = 0; i < licenses.length; i++) {
+    if (userInput.licenses === licenses[i].name) {
+      licenseSection = `${licenses[i].name}: <br />${licenses[i].link}`
+    }
+  }
+  return licenseSection;
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // Get licesnse selected by user
+  getLicenseLink(data);
+
+  return `
+# ${data.title}
+
+## Description
+${data.description}
+
+## License
+This application is covered under the ${licenseSection}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribute](#contribute)
+- [Tests](#tests)
+- [Contribute](#contribute)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contribute
+${data.contribute}
+
+## Tests
+${data.tests}
+
+## Contribute
+${data.contribute}
+
+## Questions
+For questions about this project, please refer to my personal GitHub at https://github.com/${data.github}
+You can also reach me via email at ${data.email}
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown, licenseNames }
